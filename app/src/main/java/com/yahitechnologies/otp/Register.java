7ce23fab4d1082d08423ba296ff8fd9c927d98cb
blackscure <Wekesabuyahi@gmail.com>
2021-01-12 08:49:17 +0300

    files

---
+++ app/src/main/java/com/yahitechnologies/otp/Register.java
@@ -0,0 +1,96 @@
+package com.yahitechnologies.otp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class Register extends AppCompatActivity {
+    FirebaseAuth fAuth;
+    EditText phoneNumber,codeEnter;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView state;
+    CountryCodePicker codePicker;
+    String verificationId;
+    PhoneAuthProvider.ForceResendingToken token;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        phoneNumber = findViewById(R.id.phone);
+        codeEnter = findViewById(R.id.codeEnter);
+        progressBar = findViewById(R.id.progressBar);
+        nextBtn = findViewById(R.id.nextBtn);
+        state = findViewById(R.id.state);
+        codePicker = findViewById(R.id.ccp);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!phoneNumber.getText().toString().isEmpty() && phoneNumber.getText().toString().length() == 10 ) {
+
+                    String phoneNum = "+"+codePicker.getSelectedCountryCode()+phoneNumber.getText().toString();
+                   // Log.d("clicked: -> Phone No " + phoneNum);
+                    progressBar.setVisibility(View.VISIBLE);
+                    state.setText("Sending OTp..");
+                    state.setVisibility(View.VISIBLE);
+                    requestOTP(phoneNum);
+
+                } else{
+                    phoneNumber.setError("phone Number is not Valid");
+                }
+            }
+        });
+
+    }
+
+    private void requestOTP(String phoneNum) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNum, 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                progressBar.setVisibility(View.GONE);
+                state.setVisibility(View.GONE);
+                codeEnter.setVisibility(View.VISIBLE);
+                verificationId = s;
+                token = forceResendingToken;
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(Register.this,"Cannot Create Account " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
